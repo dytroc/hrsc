1d253a61159c897905d5a5e5fb99a3abc5114adb
dytroInc <dytrocodesworkshop@gmail.com>
2022-05-17 21:32:37 +0900

    Fix server check

--- src/main/java/io/github/dytroInc/hrsc/HRSCMod.java
+++ src/main/java/io/github/dytroInc/hrsc/HRSCMod.java
@@ -3,13 +3,20 @@ package io.github.dytroInc.hrsc;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class HRSCMod implements ClientModInitializer {
+
+    private static final MinecraftClient client = MinecraftClient.getInstance();
+    private static final List<String> roinAddress = Arrays.asList("roin.tv", "roin.kr", "158.247.218.69");
 
     KeyBinding toggleChatKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "key.hrsc.togglechat",
@@ -19,12 +26,6 @@ public class HRSCMod implements ClientModInitializer {
     ));
 
     private boolean isChatHidden = true;
-
-    private boolean isRoinServer = false;
-
-    public void setRoinServer(boolean roinServer) {
-        isRoinServer = roinServer;
-    }
 
     public HRSCMod() {
         instance = this;
@@ -48,15 +49,20 @@ public class HRSCMod implements ClientModInitializer {
     }
 
     public boolean handleChat(Text text) {
-        if (isChatHidden && isRoinServer) {
+        if (isChatHidden && isRoinServer()) {
             if (text.getSiblings().size() < 1) return false;
-            for (int i = 0; i <= 1; i++) {
+            for (int i = 0; i < text.getSiblings().size(); i++) {
                 for (Text sibling : text.getSiblings().get(i).getSiblings()) {
                     if (isPlaytimeComponent(sibling.asString())) return true;
                 }
             }
         }
         return false;
+    }
+
+    private boolean isRoinServer() {
+        if (client.getCurrentServerEntry() == null) return false;
+        return roinAddress.contains(client.getCurrentServerEntry().address);
     }
 
     private static boolean isPlaytimeComponent(String text) {
